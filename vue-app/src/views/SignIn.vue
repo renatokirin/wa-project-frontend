98e623ce03cfc67341f1d6bb37740c32bfddf489
Renato Kirin <renatokirin@gmail.com>
2023-01-16 22:58:56 +0100

    Add Sign In page

--- vue-app/src/views/SignIn.vue
+++ vue-app/src/views/SignIn.vue
@@ -1,21 +1,66 @@
 <script>
-    export default {
-        components: {
-
-        },
-        data() {
-            return {
-                text: "SignIn"
-            }
-        },
-        methods: {
-            
+export default {
+    data() {
+        return {
+            email: "",
+            password: "",
+            signInFailed: false,
         }
-    };
+    },
+    methods: {
+        async submitSignIn() {
+            let json = { "email": email.value, "password": password.value };
+
+            await fetch('http://localhost:3000/api/users/auth/signIn', {
+                method: 'PATCH', credentials: 'include',
+                body: JSON.stringify(json),
+                headers: {
+                    'Content-Type': 'application/json; charset=utf-8',
+                },
+            }).then(res => res.json()).then(data => {
+                console.log(data);
+                if (!data.authenticated) this.signInFailed = true;
+                else this.$router.push('/');
+            });
+        }
+    }
+};
 </script>
 
 <template>
-    <div>
-        {{ text }}
+    <div class="content-container"
+        style="height: 100vh; display: flex; flex-direction: column; align-items: center; justify-content: center;">
+        <div class="card shadow" style="width: 400px;">
+            <div class="card-header">
+                <h1>Your blog</h1>
+            </div>
+            <div class="card-body">
+                <form style="display: flex; flex-direction: column;">
+                    <div class="form-group mt-1">
+                        <input type="email" class="form-control" id="email" placeholder="Email" v-model="email">
+                    </div>
+                    <div class="form-group mt-3">
+                        <input type="password" class="form-control" id="password" placeholder="Password"
+                            v-model="password">
+                    </div>
+                    <div class="alert alert-danger mt-4 py-1" role="alert" v-if="signInFailed">
+                        Incorrect email or password
+                    </div>
+                    <button type="button" class="btn btn-primary mt-4" v-on:click="submitSignIn()">Sign In</button>
+                    <div class="text-muted mt-5">
+                        Don't have an account?
+                        <a href="/signup">Sign up</a>
+                    </div>
+                </form>
+            </div>
+        </div>
     </div>
 </template> 
+
+<style scoped>
+.content-container {
+    background-image: radial-gradient(rgba(11, 92, 214, 0.32) 2px, transparent 2px);
+    background-size: 29px 29px;
+    background-color: white;
+}
+</style>
